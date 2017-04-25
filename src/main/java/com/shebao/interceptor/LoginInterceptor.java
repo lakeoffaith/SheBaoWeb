863a78e1888ffbb006a04bbfb0ca8818d99afe31
lakeoffaith <905663280@qq.com>
2017-04-25 23:01:21 +0800

    'loginInterceptor'

--- src/main/java/com/shebao/interceptor/LoginInterceptor.java
+++ src/main/java/com/shebao/interceptor/LoginInterceptor.java
@@ -1,19 +1,38 @@
 package com.shebao.interceptor;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class LoginInterceptor extends HandlerInterceptorAdapter  {
+import com.ijoy.service.IUserService;
+import com.ijoy.service.Resource;
 
+public class LoginInterceptor extends HandlerInterceptorAdapter  {
+	@Autowired
+	private IUserService userService;
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("进入handle");
-		return super.preHandle(request, response, handler);
+		String requestUrl=request.getRequestURI().replaceAll(request.getContextPath(), "");
+		System.out.println("requestUrl="+requestUrl);
+		if(requestUrl.contains("login") || requestUrl.contains("Login"))return true;
+		List<Resource> resource = userService.getMenusByUserId(1);
+		
+		for (Resource r : resource) {
+			System.out.println(r.getUrl());
+			if(requestUrl!=null && requestUrl.equals(r.getUrl())){
+				return true;
+			}
+		}
+		 return false;
+		//return super.preHandle(request, response, handler);
 	}
 
 	@Override
